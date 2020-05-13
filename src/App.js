226346ef68f8c962cf9182e2a922eab92d5aba0f
chief <gatezdomain@gmail.com>
2020-05-13 09:33:02 -0500

    added imports for routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { dataStore } from "./components/store/DataStore";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Main from "./components/Main";
 
-const App = (props) => {
+const App = () => {
   return (
     <Provider store={dataStore}>
-      <div className="container">
-        <Main />
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/merchant" component={Main} />
+          <Redirect to="/merchant" />
+        </Switch>
+      </Router>
     </Provider>
   );
 };
