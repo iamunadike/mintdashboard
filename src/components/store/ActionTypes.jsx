af2ff9ebab6b045c4abbca22c6fdefaf44104318
chief <gatezdomain@gmail.com>
2020-05-13 04:56:39 -0500

    created the data types and declared actions

---
+++ src/components/store/ActionTypes.jsx
@@ -0,0 +1,13 @@
+export const DataTypes = {
+    PAYMENTS: "payments",
+    ORDERS: "orders",
+  };
+
+  
+  // Declares the actions with types
+export const ActionTypes = {
+    DATA_LOAD: "load_data",
+    DATA_UPDATE: "update_data",
+    DATA_SET_PAGESIZE: "data_set_pagesize",
+  };
+  
