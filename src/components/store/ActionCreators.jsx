8b54056fe4a02056af973d6e5f225ca3f58e9bb7
chief <gatezdomain@gmail.com>
2020-05-13 05:05:04 -0500

    created the actions for the store

---
+++ src/components/store/ActionCreators.jsx
@@ -0,0 +1,27 @@
+import { ActionTypes } from "./types/ActionTypes";
+import { data as storeData } from "../../../data";
+// create this action objects with a type name (identity)
+// and the payload to be worked on
+export const loadData = (dataType) => ({
+  type: ActionTypes.DATA_LOAD,
+  payload: {
+    dataType: dataType,
+    data: storeData[dataType],
+    total: storeData[dataType].length,
+  },
+});
+
+export const updateData = (dataType, item) => ({
+  type: ActionTypes.DATA_UPDATE,
+  payload: {
+    dataType: dataType,
+    item: item,
+    data: storeData[dataType],
+  },
+});
+
+// the redux action that enables pagination
+export const setPageSize = (newSize) => ({
+  type: ActionTypes.DATA_SET_PAGESIZE,
+  payload: newSize,
+});
